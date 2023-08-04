4c778f4b44321cdcca3f9509241516a3b3c6bdee
Ambr-automation <ambreeny007@gmail.com>
2023-08-04 12:51:30 -0400

    updated to QA Env

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -1,12 +1,17 @@
 package Constants;
 
 public class Constants {
-	public static String url = "https://stagingatlas.pharma.solutions/login";
+//	//Staging Env
+//	public static String url = "https://stagingatlas.pharma.solutions/login";
+//	public static String Username = "qaautomation@pharma.solutions";
+//	public static String Password = "Test@1234";
+	
+	//QA Env
+	public static String url = "https://qa-maintenance.lha.pharma.solutions/";
 	public static String Username = "qaautomation@pharma.solutions";
-	public static String Password = "Test@1234";
+	public static String Password = "WGqb6xmV#1";
 	
 	public static String outlookUrl = "https://outlook.live.com/owa/";
 	public static String outlookUsername = "qaautomation@pharma.solutions";
-	public static String outlookPassword = "WGqb6xmV#2";
-	
+	public static String outlookPassword = "WGqb6xmV#2";	
 }
