9aa8eace3a021f9e38d1f25d35d52219ef0bb6cd
Ambr-automation <ambreeny007@gmail.com>
2023-08-04 11:55:35 -0400

    updated cradintails

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -3,7 +3,7 @@ package Constants;
 public class Constants {
 	public static String url = "https://stagingatlas.pharma.solutions/login";
 	public static String Username = "qaautomation@pharma.solutions";
-	public static String Password = "WGqb6xmV#1";
+	public static String Password = "Test@1234";
 	
 	public static String outlookUrl = "https://outlook.live.com/owa/";
 	public static String outlookUsername = "qaautomation@pharma.solutions";
